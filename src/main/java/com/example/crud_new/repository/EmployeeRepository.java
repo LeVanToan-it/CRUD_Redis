978cea75aff80991c5cda1ed12f4c3366aad524a
Le Van Toan <lvtoan.it2000@gmail.com>
2021-11-08 15:07:25 +0700

    finish crud set redis

--- src/main/java/com/example/crud_new/repository/EmployeeRepository.java
+++ src/main/java/com/example/crud_new/repository/EmployeeRepository.java
@@ -4,46 +4,53 @@ import com.example.crud_new.model.Employee;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public class EmployeeRepository {
 
     //private HashOperations hashOperations;//crud hash
-	private ListOperations listOperations; //crud list
-
-
-
+	//private ListOperations listOperations; //crud list
+	private SetOperations setOperations; //crud set
+	
     private RedisTemplate redisTemplate;
 
     public EmployeeRepository(RedisTemplate redisTemplate) {
-    	this.listOperations = redisTemplate.opsForList();
+    	this.setOperations = redisTemplate.opsForSet();
+    	//this.listOperations = redisTemplate.opsForList();
         //this.hashOperations = redisTemplate.opsForHash();
         this.redisTemplate = redisTemplate;
 
     }
 
     public void saveEmployee(Employee employee){
-    	listOperations.leftPush("LEMPLOYEE", employee);
+    	setOperations.add("SEMPLOYEE", employee);
+    	//listOperations.leftPush("LEMPLOYEE", employee);
         //hashOperations.put("EMPLOYEE", employee.getId(), employee);
     }
-    public List<Employee> findAll(){
-    	return (List<Employee>) listOperations.range("LEMPLOYEE", 0, -1);
+    public Set<Employee> findAll(){
+    	return (Set<Employee>) setOperations.members("SEMPLOYEE");
+    	//return (List<Employee>) listOperations.range("LEMPLOYEE", 0, -1);
         //return hashOperations.values("EMPLOYEE");
     }
     public Employee findById(Integer id){
-    	return (Employee) listOperations.index("LEMPLOYEE", id);
+    	return (Employee) setOperations.randomMember("SEMPLOYEE");
+    	//return (Employee) listOperations.index("LEMPLOYEE", id);
         //return (Employee) hashOperations.get("EMPLOYEE", id);
     }
 
     public void update(Employee employee){
-    	listOperations.set("LEMPLOYEE", 1, employee);
+    	setOperations.intersect("SEMPLOYEE", employee);
+    	//listOperations.set("LEMPLOYEE", 1, employee);
         //saveEmployee(employee);
     }
     public void delete(Integer id){
-    	listOperations.leftPop("LEMPLOYEE");
+    	setOperations.pop("SEMPLOYEE",id);
+    	//listOperations.leftPop("LEMPLOYEE");
         //hashOperations.delete("EMPLOYEE", id);
     }
 }
